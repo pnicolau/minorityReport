5b08b6a98c849d18255d0c433011e0dc0e83e434
Paula Nicolau <pnicolau@maclab33.local>
2016-06-16 12:53:57 -0500

    completed ap

--- minorityReport/ViewController.swift
+++ minorityReport/ViewController.swift
@@ -10,15 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var theFutureLabel: UILabel!
+    @IBOutlet var PreCogLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
+    @IBAction func onDrag(sender: UIPanGestureRecognizer)
+    {
+     let point = sender.locationInView(view)
+     theFutureLabel.center = point
+        if sender.state == .Ended
+        {
+            UIView.animateWithDuration(0.8, animations:
+                {
+                    self.theFutureLabel.center = self.view.center})
+        }
+    
+        else if CGRectContainsPoint(PreCogLabel.frame, point)
+        {
+            let myAlert = UIAlertController(title: "Minority Report", message: "PreCog Detected!", preferredStyle: UIAlertControllerStyle.Alert)
+            myAlert.addAction(UIAlertAction(title: "Arrest Them!", style: .Default, handler: nil))
+            myAlert.addAction(UIAlertAction(title: "Let Em Go!", style: .Default, handler: nil))
+            presentViewController(myAlert, animated: true, completion: nil)
+        }
+           // print("ended") test to see if it recognnizes that you have stopped touching it
+        
+        }
+        
+    
+    
+    
 
 
 }
